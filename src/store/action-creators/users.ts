e5245873629456cec3ee0ca1bc48a61dc640068f
Andrey Rekov <arekov.exceedteam@gmail.com>
2024-07-18 14:20:16 +0300

    feat: added ThunkAction type

--- src/store/action-creators/users.ts
+++ src/store/action-creators/users.ts
@@ -1,10 +1,11 @@
 import { Dispatch } from 'redux';
+import { ThunkActionApp } from 'store';
 
 import { getUsers } from 'services/users';
 import { getUsersStart, getUsersSuccess } from 'store/actions/users';
 
 export const loadUsers =
-  () =>
+  (): ThunkActionApp =>
   async (dispatch: Dispatch): Promise<void> => {
     try {
       dispatch(getUsersStart());
